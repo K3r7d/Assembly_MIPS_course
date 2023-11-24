ae6eca662d0f93f09689a60e7e2b23a50b1fc45b
Phạm Đình Bảo Duy <baoduyphamdinh@gmail.com>
2023-11-24 08:38:00 +0700

    lab 3 2.1 and 2.2

--- Lab/Lab3/2_1.asm
+++ Lab/Lab3/2_1.asm
@@ -1,18 +1,262 @@
 .data
-	msg1: .asciiz "Choose shape to calculate the surface area: 1.Rectangle Box   2. Cube   3.Cylinder   4.pyramid   5.prism   6.sphere\n"
+	msg: .asciiz "Choose the way to caculate: 1. Volume 2. Suface Area"
+	msg1: .asciiz "Choose shape to calculate 5: 1.Rectangle Box   2. Cube   3.Cylinder   4.pyramid   5.prism   6.sphere\n"
 	Input_R: .asciiz "Input R: "
 	Input_H: .asciiz "Input H: "
 	Input_A: .asciiz "Input a: "
 	Input_B: .asciiz "input b: "
 	Input_C: .asciiz "input c: "
-	Input_S: .asciiz "input slant height: "
+	Invalid_input: .asciiz "Invalid input!\n"
 	
-	ans: .a	sciiz "Surface Area: "
+	Volume_msg: .asciiz "Volume: "
+	ans: .asciiz "Surface Area: "
 	newline: .asciiz "\n"
 	two: .float 2.0
 	four: .float 4.0
 	PI: .float 3.141592
+	one_on_three: .float 0.3333
+	four_on_three: .float 1.3333
 .text
+	li $v0, 4
+	la $a0, msg
+	syscall
+	
+	li $v0, 5
+	syscall
+	beq $v0, 2, SurfaceArea
+	beq $v0, 1, Volume
+	
+	li $v0, 4
+	la $a0, Invalid_input
+	syscall
+	
+	Volume:
+	
+	#input
+	li $v0, 4
+	la $a0, msg1
+	syscall
+	
+	li $v0, 5
+	syscall
+	move $s0, $v0
+	
+	beq $s0, 1, Rectangle_V
+	beq $s0, 2, Cube_V
+	beq $s0, 3, Cylinder_V
+	beq $s0, 4, pyramid_V
+	beq $s0, 5, prism_V
+	beq $s0, 6, sphere_V
+	
+	li $v0, 4
+	la $a0, Invalid_input
+	syscall
+	
+	
+	Rectangle_V:
+	
+	li $v0, 4
+	la $a0, Input_A
+	syscall
+	
+	li $v0, 6
+	syscall
+	mov.s $f1, $f0
+	
+	li $v0, 4
+	la $a0, newline
+	syscall
+	
+	li $v0, 4
+	la $a0, Input_B
+	syscall
+	
+	li $v0, 6
+	syscall
+	mov.s $f2, $f0
+	
+	li $v0, 4
+	la $a0, newline
+	syscall
+	
+	li $v0, 4
+	la $a0, Input_C
+	syscall
+	
+	li $v0, 6
+	syscall
+	mov.s $f3, $f0
+	
+	li $v0, 4
+	la $a0, newline
+	syscall
+	
+	mul.s $f4, $f1, $f2
+	mul.s $f4, $f4, $f3
+	
+	mov.s $f12,$f4
+	j answer_V
+Cube_V:
+	li $v0, 4
+	la $a0, Input_A
+	syscall
+	
+	li $v0, 6
+	syscall
+	mov.s $f1, $f0
+	
+	li $v0, 4
+	la $a0, newline
+	
+	mul.s $f4, $f1,$f1
+	mul.s $f4, $f4,$f1
+
+	mov.s $f12, $f4
+	j answer_V
+Cylinder_V:
+	li $v0, 4
+	la $a0, Input_R
+	syscall
+	
+	li $v0, 6
+	syscall
+	mov.s $f1, $f0
+	
+	li $v0, 4
+	la $a0, newline
+	syscall
+	
+	li $v0, 4
+	la $a0, Input_H
+	syscall
+	
+	li $v0, 6
+	syscall
+	mov.s $f2, $f0
+	
+	li $v0, 4
+	la $a0, newline
+	syscall
+	
+	l.s $f3 ,PI
+	l.s $f4, two
+	
+	mul.s $f1, $f1, $f1
+	mul.s $f5, $f3, $f1
+	mul.s $f5, $f5, $f2
+	
+	mov.s $f12, $f5
+	
+	j answer_V
+	
+	
+pyramid_V:
+	li $v0, 4
+	la $a0, Input_A
+	syscall
+	
+	li $v0, 6
+	syscall
+	mov.s $f1, $f0
+	
+	li $v0, 4
+	la $a0, Input_H
+	syscall
+	
+	li $v0, 6
+	syscall
+	mov.s $f2, $f0
+
+	l.s $f3 one_on_three
+	
+	mul.s $f4, $f3,$f2
+	mul.s $f1, $f1, $f1
+	mul.s $f4, $f4, $f1
+	
+	mov.s $f12, $f4	
+	
+	j answer_V
+	
+	
+prism_V:
+	li $v0, 4
+	la $a0, Input_A
+	syscall
+	
+	li $v0, 6
+	syscall
+	mov.s $f1, $f0
+	
+	li $v0, 4
+	la $a0, Input_B
+	syscall
+	
+	li $v0, 6
+	syscall
+	mov.s $f2, $f0
+	
+	li $v0, 4
+	la $a0, Input_C
+	syscall
+	
+	li $v0, 6
+	syscall
+	mov.s $f3, $f0
+	
+	li $v0, 4
+	la $a0, Input_H
+	syscall
+	
+	li $v0, 6
+	syscall
+	mov.s $f11, $f0
+	
+	add.s $f4, $f1, $f2
+	add.s $f4, $f4, $f3
+	l.s $f5, two
+	div.s $f6, $f4, $f5	#p
+	sub.s $f7, $f6, $f1
+	sub.s $f8, $f6, $f2
+	sub.s $f9, $f6, $f3
+	mul.s $f10, $f6,$f7
+	mul.s $f10, $f10, $f8
+	mul.s $f10, $f10, $f9
+	sqrt.s $f10, $f10
+	mul.s $f12, $f11, $f10
+	
+	j answer_V
+	
+sphere_V:
+	li $v0, 4
+	la $a0, Input_R
+	syscall
+	
+	li $v0, 6
+	syscall
+	mov.s $f1, $f0
+	
+	l.s $f2 ,PI
+	l.s $f3,four_on_three
+	
+	mul.s $f5, $f1, $f1
+	mul.s $f5, $f1, $f5
+	mul.s $f4, $f2, $f3
+	mul.s $f12, $f4,$f5
+	
+	j answer_V
+answer_V:
+	li $v0, 4
+	la $a0, Volume_msg
+	syscall
+	
+	li $v0,2 
+	syscall
+	
+	j exit
+	
+	
+	
+	SurfaceArea:
 	#input
 	li $v0, 4
 	la $a0, msg1
@@ -28,6 +272,11 @@
 	beq $s0, 4, pyramid
 	beq $s0, 5, prism
 	beq $s0, 6, sphere
+	
+	li $v0, 4
+	la $a0, Invalid_input
+	syscall
+	j SurfaceArea
 	
 Rectangle:
 	
--- Lab/Lab3/2_2.asm
+++ Lab/Lab3/2_2.asm
@@ -1,14 +1,10 @@
 .data
-	.msg: "Please input your ID: "
+	msg:.asciiz "Please input your ID (7 digits): "
 	
-	a: .float
-	b: .float
-	c: .float
-	d: .float
-	e: .float
-	u: .float
-	v: .float
-	
+	one: .float 1.0
+	three:.float 3.0
+	four: .float 4.0
+	five: .float 5.0
 .text
 	li $v0, 4
 	la $a0, msg
@@ -16,6 +12,116 @@
 	
 	li $v0, 5
 	syscall
-	move $s0, $a0
+	move $s0, $v0
+	
+	li $s1, 10
+	
+	div $s0, $s1
+	mfhi $t7
+	mflo $s0
+	
+	div $s0, $s1
+	mfhi $t6
+	mflo $s0
+	
+	div $s0, $s1
+	mfhi $t5
+	mflo $s0
+	
+	div $s0, $s1
+	mfhi $t4
+	mflo $s0
+	
+	div $s0, $s1
+	mfhi $t3
+	mflo $s0
+	
+	div $s0, $s1
+	mfhi $t2
+	mflo $s0
+	
+	div $s0, $s1
+	mfhi $t1
+	mflo $s0
+	#e
+	mtc1 $t7, $f7
+	cvt.s.w $f7, $f7
+	#v
+	mtc1 $t6, $f6
+	cvt.s.w $f6, $f6
+	#u
+	mtc1 $t5, $f5
+	cvt.s.w $f5, $f5
+	
+	#d
+	mtc1 $t4, $f4
+	cvt.s.w $f4, $f4
+	
+	#c
+	mtc1 $t3, $f3
+	cvt.s.w $f3, $f3
+	
+	#b
+	mtc1 $t2, $f2
+	cvt.s.w $f2, $f2
+	
+	#a
+	mtc1 $t1, $f1
+	cvt.s.w $f1, $f1
+	
+	mul.s $f7, $f7, $f7 #e^2
+	
+	l.s $f8, one
+	div.s $f16, $f8, $f7 #1/e^2
+	
+	sub.s $f9, $f5, $f6
+	mul.s $f9, $f9, $f4 #d(u-v)
+
+	l.s $f8, three
+	
+	mul.s $f10, $f5, $f5 #u^2
+	mul.s $f10, $f5, $f10 #u^3
+	mul.s $f11, $f6, $f6
+	mul.s $f11, $f11, $f6#v^3
+	sub.s $f11, $f10, $f11
+	div.s $f13, $f11, $f8 
+	mul.s $f13, $f13, $f3 #c(u^3-v^3)/3
+	
+	l.s $f8, four
+	
+	mul.s $f10, $f5, $f5
+	mul.s $f10, $f10, $f10
+	mul.s $f11, $f6, $f6
+	mul.s $f11, $f11, $f11
+	sub.s $f11, $f10, $f11
+	div.s $f11, $f11, $f8
+	mul.s $f14, $f11, $f2
+	
+	l.s $f8, five
+	
+	mul.s $f10, $f5, $f5
+	mul.s $f10, $f10, $f10
+	mul.s $f10, $f10, $f5
+	mul.s $f11, $f6, $f6
+	mul.s $f11, $f11, $f11
+	mul.s $f11, $f11, $f6
+	sub.s $f11, $f10, $f11
+	div.s $f11, $f11, $f8
+	mul.s $f15, $f11, $f1
+	
+	add.s $f12, $f13, $f14
+	add.s $f12, $f12, $f15
+	add.s $f12, $f12, $f9
+	mul.s $f12, $f12, $f16
+	
+	li $v0, 2
+	syscall
+	
+	
+	
+	li $v0, 10
+	syscall
+	
+	
 	
 	
